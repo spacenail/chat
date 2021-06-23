27d70e21b507ae89bfa4501b8c705ea6f921a659
Artem Meshcherov <spacenail@gmail.com>
2021-06-23 23:08:31 +0300

    v0.1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    TextArea textArea;
+    @FXML
+    TextField inputText;
+
+    public void btnClickAction(ActionEvent actionEvent) {
+        textArea.appendText(inputText.getText() + "\n");
+    }
 }
