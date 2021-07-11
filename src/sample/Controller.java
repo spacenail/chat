d9fadeb37f04071495aba1fcde8ab61bee3931d8
Artem Meshcherov <spacenail@gmail.com>
2021-07-11 21:12:18 +0300

    v1.0

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -13,5 +13,16 @@ public class Controller {
 
     public void btnClickAction(ActionEvent actionEvent) {
         textArea.appendText(inputText.getText() + "\n");
+        inputText.clear();
     }
+
+    public void btnClickActionClear(ActionEvent actionEvent) {
+        textArea.clear();
+    }
+
+    public void enterAction(ActionEvent actionEvent){
+        textArea.appendText(inputText.getText() + "\n");
+        inputText.clear();
+    }
+
 }
